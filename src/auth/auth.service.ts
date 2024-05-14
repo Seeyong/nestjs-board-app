e8011afeda831be845d5190257b6008c81b06dca
Seeyong <seeyong987@gmail.com>
2024-05-14 19:25:07 +0900

    setting basic signIn func

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { AuthCredentialDto } from './dto/auth-credential.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +10,17 @@ export class AuthService {
 
   async singUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialDto);
+  }
+
+  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+    const { username, password } = authCredentialDto;
+    const user: Promise<User> = this.userRepository.findOne({
+      where: { username },
+    });
+    if (user && (await bcrypt.compare(password, (await user).password))) {
+      return 'login Success';
+    } else {
+      throw new UnauthorizedException('login Failed!');
+    }
   }
 }
