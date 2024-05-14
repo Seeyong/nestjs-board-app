2c6a0511d0608d83aa33e40fe99f1fd5dff2cac6
Seeyong <seeyong987@gmail.com>
2024-05-14 18:51:03 +0900

    set username in signup unique

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
@@ -15,6 +19,14 @@ export class UserRepository extends Repository<User> {
       username,
       password,
     });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing Username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
