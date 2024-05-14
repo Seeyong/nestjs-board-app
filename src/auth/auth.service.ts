9a935d6aa724ec0d2e5ae831a79dc6f900619e62
Seeyong <seeyong987@gmail.com>
2024-05-14 15:50:14 +0900

    add auth cred dto and basic signup func

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
   constructor(private userRepository: UserRepository) {}
+
+  async singUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialDto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(datasource: DataSource) {
     super(User, datasource.createEntityManager());
+  }
+
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({
+      username,
+      password,
+    });
+    await this.save(user);
   }
 }
