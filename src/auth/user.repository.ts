b75083566d28d75423373ef0862cd88aabedebd3
Seeyong <seeyong987@gmail.com>
2024-05-14 19:03:54 +0900

    apply bcryptjs for password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,6 +6,7 @@ import {
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -15,9 +16,13 @@ export class UserRepository extends Repository<User> {
 
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
+
+    const salt: string = await bcrypt.genSalt();
+    const hashedPassword: string = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
     try {
       await this.save(user);
