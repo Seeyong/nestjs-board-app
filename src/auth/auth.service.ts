182404ac8db5006fabffb801071da78ebb84590d
Seeyong <seeyong987@gmail.com>
2024-05-20 19:08:43 +0900

    apply jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,22 +3,30 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async singUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user: Promise<User> = this.userRepository.findOne({
       where: { username },
     });
     if (user && (await bcrypt.compare(password, (await user).password))) {
-      return 'login Success';
+      const payload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('login Failed!');
     }
